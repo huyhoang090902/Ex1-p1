1a13c693ae553377282df07a721865ab9e2fc24f
huyhoang090902 <2024802010419@student.tdmu.edu.vn>
2024-04-20 13:58:15 +0700

    first commit

---
+++ App.js
@@ -0,0 +1,26 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+import Bai1 from './src/Bai1';
+import Bai2 from './src/Bai2';
+import Bai3 from './src/Bai3';
+import Bai4 from './src/Bai4';
+import Bai5 from './src/Bai5';
+import Bai6 from './src/Bai6';
+import Bai7 from './src/Bai7';
+import Bai8 from './src/Bai8';
+const  App=()=>{
+  return (
+    <Bai1/>
+
+  );
+}
+export default App;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
